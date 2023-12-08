e67967e7a2f8a4380011e653b1b9ac9d5143f6e6
Keenan Schott <keenanmschott28@gmail.com>
2023-12-07 23:07:44 -0700

    Day 8 Complete!

---
+++ Day-8/Part-2/code.py
@@ -0,0 +1,41 @@
+from math import gcd
+
+with open('./Day-8/input.txt', 'r') as f:
+    data = f.read().split('\n')
+
+tree = {}
+starts = []
+for i in range(2, len(data)):
+    source, _, left, right = data[i].split()
+    left, right = left[1:4], right[0:3]
+    if source[2] == 'A':
+        starts.append(source)
+    if source not in tree:
+        tree[source] = []
+    if left not in tree:
+        tree[left] = []
+    if right not in tree:
+        tree[right] = []
+    tree[source].append(left)
+    tree[source].append(right)
+
+for i in range(len(starts)):
+    start = starts[i]
+    steps = 0
+    while True:
+        current_instruction = data[0][steps % len(data[0])]
+        if current_instruction == 'L':
+            new_source = tree[start][0]
+        else:
+            new_source = tree[start][1]
+        steps += 1
+        if new_source[2] == 'Z':
+            starts[i] = steps
+            break
+        start = new_source
+
+lcm = 1
+for steps in starts:
+    lcm = lcm * steps // gcd(lcm, steps)
+
+print(lcm)
