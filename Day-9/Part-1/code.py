ce22b2103af54e0e16ba7c02c89d325b1baf8f08
Keenan Schott <keenanmschott28@gmail.com>
2023-12-08 22:34:36 -0700

    Day 9 Complete!

---
+++ Day-9/Part-1/code.py
@@ -0,0 +1,20 @@
+with open('./Day-9/input.txt', 'r') as f:
+    data = f.read().split('\n')
+
+total = 0
+for line in data:
+    histories = [list(map(int, line.split(' ')))]
+    while True:
+        history = []
+        for i in range(len(histories[-1]) - 1):
+            history.append(histories[-1][i + 1] - histories[-1][i])
+        histories.append(history)
+        if any(element != 0 for element in history):
+            continue
+        break
+    histories = histories[::-1]
+    for i in range(1, len(histories)):
+        histories[i].append(histories[i][-1] + histories[i - 1][-1])
+    total += histories[-1][-1]
+
+print(total)
